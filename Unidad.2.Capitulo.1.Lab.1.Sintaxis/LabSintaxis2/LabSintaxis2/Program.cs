cc710ab79d1e0c386eb98a5d5f34c66823951871
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2021-04-22 10:26:17 -0300

    Complete Ej 1

--- Unidad.2.Capitulo.1.Lab.1.Sintaxis/LabSintaxis2/LabSintaxis2/Program.cs
+++ Unidad.2.Capitulo.1.Lab.1.Sintaxis/LabSintaxis2/LabSintaxis2/Program.cs
@@ -10,6 +10,41 @@ namespace LabSintaxis2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Por favor, ingrese un texto.");
+            string inputTexto = Console.ReadLine();
+            Console.WriteLine();
+            if (string.IsNullOrEmpty(inputTexto))
+            {
+                Console.WriteLine("Mensaje no ingresado. La aplicación se cerrará.");
+            } else
+            {
+                Console.WriteLine("MENÚ DE OPCIONES");
+                Console.WriteLine("1.   Mostrar la frase ingresada en MAYÚSCULAS");
+                Console.WriteLine("2.   Mostrar la frase ingresada en minúsculas");
+                Console.WriteLine("3.   Mostrar la cantidad de caracteres que posee el texto");
+                Console.Write("Ingresar la opcion deseada: ");
+                ConsoleKeyInfo opcion = Console.ReadKey();
+                Console.WriteLine();
+                Console.WriteLine();
+                if (opcion.Key == ConsoleKey.D1)
+                {
+                    Console.WriteLine(inputTexto.ToUpper());
+                }
+                else if (opcion.Key == ConsoleKey.D2)
+                {
+                    Console.WriteLine(inputTexto.ToLower());
+                }
+                else if (opcion.Key == ConsoleKey.D3)
+                {
+                    Console.WriteLine("El texto contiene "+inputTexto.Length+" caracteres");
+                }
+                else
+                {
+                    Console.WriteLine("Opción incorrecta. El programa se cerrará.");
+                }
+            }
+            Console.WriteLine("Pulsa para salir.");
+            Console.ReadKey();
         }
     }
 }
