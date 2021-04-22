3f13bbdf8971d391347a15c3e9f21fcce377f882
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2021-04-22 10:59:39 -0300

    Complete Ej 3

---
+++ Unidad.2.Capitulo.1.Lab.1.Sintaxis/LabSintaxis3/LabSintaxis3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantIteraciones = 5;
+            string[] arreglo = new string[cantIteraciones];
+            Console.WriteLine("Ingrese texto.");
+            for (int i = 0; i < cantIteraciones; i++)
+            {
+                arreglo[i] = Console.ReadLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Texto ingresado:");
+            for (int i = cantIteraciones-1; i >= 0; i--)
+            {
+                Console.WriteLine(arreglo[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Pulse para salir.");
+            Console.ReadKey();
+        }
+    }
+}
