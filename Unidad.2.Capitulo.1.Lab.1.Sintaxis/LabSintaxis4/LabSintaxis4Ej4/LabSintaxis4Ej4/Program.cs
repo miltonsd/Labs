50389e0b8f69db416c0562e2b0a8a028e81c2c88
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2021-04-22 21:48:21 -0300

    Complete Ej 4

---
+++ Unidad.2.Capitulo.1.Lab.1.Sintaxis/LabSintaxis4/LabSintaxis4Ej4/LabSintaxis4Ej4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis4Ej4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lista de números pares entre el 1 y el 100:");
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.Write("\nPulse para salir.");
+            Console.ReadKey();
+        }
+    }
+}
