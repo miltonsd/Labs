512a2af528927411b897f6bed6e4163307a841ee
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2021-04-25 01:32:37 -0300

    Complete Ej 7

---
+++ Unidad.2.Capitulo.1.Lab.1.Sintaxis/LabSintaxis4/LabSintaxis4Ej7/LabSintaxis4Ej7/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis4Ej7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresar el número máximo en donde se buscarán primos gemelos: ");
+            int nro = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            for (int i = 2; i < nro; i++)
+            {
+                bool esPrimo = true;
+                // Búsqueda del primer número primo
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        esPrimo = false;
+                        break;
+                    }
+                }
+                if (esPrimo)
+                {
+                    // Evaluar si segundo número es primo
+                    for (int j = 2; j < (i + 2); j++)
+                    {
+                        if ((i + 2) % j == 0)
+                        {
+                            esPrimo = false;
+                            break;
+                        }
+                    }
+                    if (esPrimo)
+                    {
+                        Console.WriteLine("Los números "+i+" y "+(i+2)+" son primos gemelos.");
+                    }
+                }
+            }
+            Console.Write("\nPulse para salir.");
+            Console.ReadKey();
+        }
+    }
+}
