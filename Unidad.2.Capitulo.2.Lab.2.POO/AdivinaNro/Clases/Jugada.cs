cf67b07c3b89df813e59d74fab5e7bdcd1c5cf8c
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2021-04-30 02:59:48 -0300

    Ej5 Completado

---
+++ Unidad.2.Capitulo.2.Lab.2.POO/AdivinaNro/Clases/Jugada.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    class Jugada
+    {
+        private bool _adivino;
+        private int _intentos;
+        private int _numero;
+
+        public Jugada(int maxNumero)
+        {
+            Adivino = false;
+            Random rnd = new Random();
+            Numero = rnd.Next(maxNumero);
+        }
+
+        public bool Adivino { get; set; }
+
+        public int Intentos
+        {
+            get => _intentos;
+            set => _intentos = value;
+        }
+
+        public int Numero
+        {
+            get
+            {
+                return _numero;
+            }
+            set
+            {
+                _numero = value;
+            }
+        }
+
+        public void Comparar(int nro)
+        {
+            Intentos += 1;
+            if (nro == Numero)
+            {
+                Console.WriteLine("Correcto. Adivinaste el número!");
+                Adivino = true;
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto! Intenta de nuevo.");
+            }
+        }
+    }
+}
