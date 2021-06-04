3ef34faefd1a4a38e294a99fec141a36b846336f
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2021-06-04 16:26:54 -0300

    Complete lab

--- Unidad.2.Extra.Lab.3.LINQ/ConsoleApp/FuncionesLINQ/FuncionesLinq.cs
+++ Unidad.2.Extra.Lab.3.LINQ/ConsoleApp/FuncionesLINQ/FuncionesLinq.cs
@@ -8,22 +8,49 @@ namespace FuncionesLINQ
     {
         public IEnumerable<string> ObtenerProvinciasQueEmpiezanConDeterminadasLetras(IEnumerable<string> provincias)
         {
-            throw new NotImplementedException();
+            var provinciasQueEmpiezanCon = from provincia in provincias
+                                           where provincia.StartsWith('S') || provincia.StartsWith('T')
+                                           select provincia;
+            return provinciasQueEmpiezanCon;                        
         }
 
         public IEnumerable<int> ObtenerNumerosMayoresA20(IEnumerable<int> numeros)
         {
-            throw new NotImplementedException();
+            var numerosMayores = from numero in numeros
+                                 where numero > 20
+                                 select numero;
+            return numerosMayores;
         }
 
         public IEnumerable<int> ObtenerCodigoPostalDeCiudadesQueTenganEnSuNombreTresCarateresDeterminados(IEnumerable<Ciudad> ciudades, string ciudad)
         {
-            throw new NotImplementedException();
+            var codigosPostales = from city in ciudades
+                                  where city.Nombre.ToLower().Contains(ciudad)
+                                  select city.CodigoPostal;
+            return codigosPostales;
         }
 
         public IEnumerable<Empleado> AgregarEmpleadoListaDevolviendolaOrdenadaPorSueldo(IEnumerable<Empleado> empleados, IEnumerable<Empleado> empleadosParaAgregar, string order)
         {
-            throw new NotImplementedException();
+            var empleadosCombinados = (from empleado in empleados
+                                       select empleado)
+                                       .Concat(from empleadoAgregado in empleadosParaAgregar
+                                               select empleadoAgregado);
+
+            if (order == "ASC")
+        	{
+                var listaEmpleados = from empleado in empleadosCombinados
+                                     orderby empleado.Sueldo ascending
+                                     select empleado;
+                return listaEmpleados;
+            }
+            else
+	        {
+                var listaEmpleados = from empleado in empleadosCombinados
+                                     orderby empleado.Sueldo descending
+                                     select empleado;
+                return listaEmpleados;
+            }
         }
     }
 }
