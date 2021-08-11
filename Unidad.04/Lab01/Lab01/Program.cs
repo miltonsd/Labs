66eba48bfae424f6b7ed90bb3e84a001ddd366f6
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2021-08-11 02:45:45 -0300

    Complete Unit04 Lab01 Txt + XML

--- Unidad.04/Lab01/Lab01/Program.cs
+++ Unidad.04/Lab01/Lab01/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Xml;
 
 namespace Lab01
 {
@@ -11,10 +12,13 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            leer();
+            Console.WriteLine("Presione una tecla para generar el archivo agendaxml.xml con los datos de agenda.txt");
             Console.ReadKey();
-            escribir();
-            leer();
+            escribirXML();
+            Console.WriteLine("Archivo agendaxml.xml generado correctamente\n\nPresione una tecla para ver su contenido");
+            Console.ReadKey();
+            Console.WriteLine();
+            leerXML();
             Console.ReadKey();
         }
 
@@ -63,6 +67,63 @@ namespace Lab01
                 rta = Console.ReadLine();
             }
             escritor.Close();
+        }
+
+        private static void leerXML()
+        {
+            XmlTextReader lectorXML = new XmlTextReader("agendaxml.xml");
+            string tagAnterior = "";
+            while (lectorXML.Read())
+            {
+                if (lectorXML.NodeType == XmlNodeType.Element)
+                {
+                    tagAnterior = lectorXML.Name;
+                }
+                else if (lectorXML.NodeType == XmlNodeType.Text)
+                {
+                    Console.WriteLine(tagAnterior + ": " + lectorXML.Value);
+                }
+            }
+            lectorXML.Close();
+        }
+
+        private static void escribirXML()
+        {
+            XmlTextWriter escritorXML = new XmlTextWriter("agendaxml.xml",null);
+            escritorXML.Formatting = Formatting.Indented; //esto no es necesario pero hará más facil para nosotros leer el contenido del archivo
+            escritorXML.WriteStartDocument(true);
+            escritorXML.WriteStartElement("DocumentElement");
+            //este elemento no es necesario lo agregaremos para compatibilidad con los xml generados en el siguiente laboratorio
+            StreamReader lector = File.OpenText("agenda.txt");
+            string linea;
+            do
+            {
+                linea = lector.ReadLine();
+                if (linea != null)
+                {
+                    string[] valores = linea.Split(';');
+                    escritorXML.WriteStartElement("contactos");
+                    escritorXML.WriteStartElement("nombre");
+                    escritorXML.WriteValue(valores[0]);
+                    escritorXML.WriteEndElement(); //cerramos el tag de nombre
+                    escritorXML.WriteStartElement("apellido");
+                    escritorXML.WriteValue(valores[1]);
+                    escritorXML.WriteEndElement(); //cerramos el tag de apellido
+                    escritorXML.WriteStartElement("email");
+                    escritorXML.WriteValue(valores[2]);
+                    escritorXML.WriteEndElement(); //cerramos el tag de email
+                    escritorXML.WriteStartElement("telefono");
+                    escritorXML.WriteValue(valores[3]);
+                    escritorXML.WriteEndElement(); //cerramos el tag de telefono
+                    escritorXML.WriteEndElement(); //cerramos el tag de contactos
+                }
+            }
+            while (linea != null);
+            escritorXML.WriteEndElement(); //cerramos el tag de DocumentElement
+            escritorXML.WriteEndDocument();
+            escritorXML.Close();
+
+            lector.Close();
         }
     }
 }
