11ff6f5b43857200e305bf4bc2ec75d84caa89c6
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2021-08-12 02:39:37 -0300

    Complete Unit04 Lab03 Ej06

--- Unidad.04/Lab03/Ejercicio4/Ejercicio4/Program.cs
+++ Unidad.04/Lab03/Ejercicio4/Ejercicio4/Program.cs
@@ -48,6 +48,54 @@ namespace Ejercicio4
                 Console.WriteLine(idempresa + " - " + nombreempresa);
             }
             Console.ReadLine();
+
+            //Primero indico el CustomerID que deseo modificar
+            Console.Write("Escriba el CustomerID que desea modificar: ");
+            string custid = Console.ReadLine();
+
+            //Luego me traigo una coleccion de datarows que contengan ese CustomerID
+            DataRow[] rwEmpresas = dtEmpresas.Select("CustomerID = '" + custid + "'");
+            if (rwEmpresas.Length != 1) //Si no encuentro nada entonces salgo
+            {
+                Console.WriteLine("CustomerID no encontrado");
+                Console.ReadLine();
+                return;
+            }
+
+            //Me traigo el primer datarow de la colección
+            DataRow rowMiEmpresa = rwEmpresas[0];
+            string nombreActual = rowMiEmpresa["CompanyName"].ToString();
+            //Muestro en consola el nombre del CustomerID seleccionado
+            Console.WriteLine("Nombre actual de la empresa: " + nombreActual);
+            //Solicito que escriba un nuevo nombre
+            Console.Write("Escriba el nuevo nombre: ");
+            string nuevoNombre = Console.ReadLine();
+
+            //Llamo al método BeginEdit del datarow para iniciar los cambios
+            rowMiEmpresa.BeginEdit();
+            //Modifico el valor del campo CompanyName
+            rowMiEmpresa["CompanyName"] = nuevoNombre;
+            //Finalizo la edicion llamando al método EndEdit()
+            rowMiEmpresa.EndEdit();
+
+            //Ahora creo un objeto Command que utilizare para
+            //guardar los cambios en la base de datos
+            SqlCommand updCommand = new SqlCommand();
+            //Le indico la conexion
+            updCommand.Connection = myconn;
+            //Le indico la cadena TSQL
+            updCommand.CommandText =
+                "UPDATE Customers SET CompanyName = @CompanyName WHERE CustomerID = @CustomerID";
+            //Indico los parametros que estoy utilizando.
+            //Como asi tambien, el tipo de dato, la longitud del dato
+            //el nombre del campo del datatable
+            updCommand.Parameters.Add("@CompanyName", SqlDbType.NVarChar, 50, "CompanyName");
+            updCommand.Parameters.Add("@CustomerID", SqlDbType.NVarChar, 5, "CustomerID");
+
+            //Ahora adjunto el objeto updCommand al DataAdapter
+            myadap.UpdateCommand = updCommand;
+            //Por último llamo al método .Update del DataAdapter
+            myadap.Update(dtEmpresas);
         }
     }
 }
